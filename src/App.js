7baf02e0bc78b89656047c7a56782376ce8000f7
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2023-08-13 12:13:20 +0000

    final change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+  const itemsSold = [
+    { id: 1, name: "iPhone 14", price: 1200, qty: 22 },
+    { id: 2, name: "iPad Pro", price: 800, qty: 18 },
+    { id: 3, name: "Macbook Air", price: 1500, qty: 7 },
+    { id: 4, name: "Samsung S23", price: 1100, qty: 16 },
+    { id: 5, name: "Dell Inspiron 5590", price: 1200, qty: 5 }
+  ];
+
+  return (
+    <>
+      <h1>Record of sold items</h1>
+      <table border="1px">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Selling price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+            {itemsSold.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.qty}</td>
+              </tr>
+            ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Total Revenue</td>
+            <td>$5800</td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  );
+};
 export default App;
